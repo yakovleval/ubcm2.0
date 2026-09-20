081e8f237269cb203bd61fb02765db550db2b05e
yakovleval <samya89_muravlenko@mail.ru>
2026-09-20 18:55:14 +0300

    unify main.c

--- src/main.c
+++ src/main.c
@@ -2,25 +2,18 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    if (argc < 3) {
-        fprintf(stderr, "Usage: %s <procedure> <rs>\n", argv[0]);
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s <procedure> <rs>\n", argv[0]);
         return 1;
     }
-    
+
     VM *vm = vm_create();
 
-    vm_load_procedure(vm, 1, "program.ubc");   // main
-    vm_load_rs(vm, 2, "standard.rs");          // РС
-    vm_load_procedure(vm, 5, "subproc.ubc");   // subproc
-
-    vm_set_uint64(vm, 10, 5);
-    vm_set_uint64(vm, 11, 7);
-    vm_create_register(vm, 12, 64);
+    vm_load_procedure(vm, 1, argv[1]);
+    vm_load_rs(vm, 2, argv[2]);
 
     vm_start(vm, 1, 2);
     vm_run(vm);
-
-    printf("Result r12 = %llu\n", (unsigned long long)vm_get_uint64(vm, 12));
 
     vm_free(vm);
     return 0;
